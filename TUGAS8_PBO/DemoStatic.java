8fbb7a89406885b0b3b4a0ec10daf5f209108537
Apriani magfira <57305690+13020180058@users.noreply.github.com>
2020-05-04 09:44:41 +0800

    Add files via upload

---
+++ TUGAS8_PBO/DemoStatic.java
@@ -0,0 +1,25 @@
+
+
+package variabelStatic;
+
+/*NAMA ; APRIANI MAGFIRA
+STAMBUK : 13020180058
+TANGGAL/WAKTU ;01 MEI 2020 / 21.45
+ */
+
+public class DemoStatic {
+  // Deklarasi static Variabel
+  private static String nama;
+  private static int daya;
+
+  public static void main(String[] args){
+    /**
+     * Inisialisasi variabel nama dan daya,
+     * Menampilkan nama dan daya lampu
+     */
+    nama = "Bohlam";
+    daya = 4;
+    System.out.println("Nama lampu\t\t: " + nama);
+    System.out.println("Daya tahan lampu\t: " + daya + " Bulan");
+   }
+}
---
+++ TUGAS8_PBO/methodStatic.java
@@ -0,0 +1,16 @@
+
+/*NAMA ; APRIANI MAGFIRA
+STAMBUK : 13020180058
+TANGGAL/WAKTU ;01 MEI 2020 / 21.50 
+ */
+
+
+public class methodStatic{
+  public static void main(String[] args) {
+    display();
+  }
+  static void display() {
+    System.out.println("Java is my favorite programming language.");
+  }
+}
+
